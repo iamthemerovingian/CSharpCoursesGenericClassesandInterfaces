fb9f02edcc3bb4ef7944903b7e7c8f66b5a574ad
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-10-26 13:29:15 +0700

    Moved the Empoyee Comparer to its own .CS file.

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -3,23 +3,6 @@ using System.Collections.Generic;
 
 namespace CollectIt
 {
-    public class EmployeeComparer : IEqualityComparer<Employee>, IComparer<Employee>
-    {
-        public int Compare(Employee x, Employee y)
-        {
-            return string.Compare(x.Name, y.Name);
-        }
-
-        public bool Equals(Employee x, Employee y)
-        {
-            return string.Equals(x.Name, y.Name);
-        }
-
-        public int GetHashCode(Employee obj)
-        {
-            return obj.Name.GetHashCode();
-        }
-    }
     class Program
     {
         static void Main(string[] args)
