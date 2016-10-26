c4d5d9e44dbbffcedebeae22d14d835337f12a53
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-10-26 13:26:55 +0700

    Added a new class to help the sorted dictionary compare our objects and prevent duplicacy.
    We also changed the hashset to a sorted set to allow us to sort the employees in the dictionary aplabetically both by department and by name.
    
    To implement the sorting by name we added the IComparer interface to the Employee Comparer class in order to tell the SortedSet how to arrange employees.

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -3,21 +3,61 @@ using System.Collections.Generic;
 
 namespace CollectIt
 {
+    public class EmployeeComparer : IEqualityComparer<Employee>, IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+
+        public bool Equals(Employee x, Employee y)
+        {
+            return string.Equals(x.Name, y.Name);
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var employeesByName = new SortedList<string, List<Employee>>();
+            var departments = new SortedDictionary<string, SortedSet<Employee>>();
 
-            employeesByName.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
-            employeesByName.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+            departments.Add("Sales", new SortedSet<Employee>(new EmployeeComparer()));
+            departments["Sales"].Add(new Employee { Name = "Joy" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
+            departments["Sales"].Add(new Employee { Name = "Dani" });
 
-            foreach (var item in employeesByName)
+            departments.Add("Engineering", new SortedSet<Employee>(new EmployeeComparer()));
+            departments["Engineering"].Add(new Employee { Name = "Scott" });
+            departments["Engineering"].Add(new Employee { Name = "Alex" });
+            departments["Engineering"].Add(new Employee { Name = "Dani" });
+
+
+
+            foreach (var department in departments)
             {
-                Console.WriteLine("The count of employees for {0} is {1}", 
-                            item.Key, item.Value.Count
-                        );
+                Console.WriteLine(department.Key);
+                foreach (var employee in department.Value)
+                {
+                    Console.WriteLine("\t" + employee.Name);
+                }
             }
+
+            //var employeesByName = new SortedList<string, List<Employee>>();
+
+            //employeesByName.Add("Sales", new List<Employee> { new Employee(), new Employee(), new Employee() });
+            //employeesByName.Add("Engineering", new List<Employee> { new Employee(), new Employee() });
+
+            //foreach (var item in employeesByName)
+            //{
+            //    Console.WriteLine("The count of employees for {0} is {1}", 
+            //                item.Key, item.Value.Count
+            //            );
+            //}
 
         }
     }
