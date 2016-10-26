ae04a2b974c44f0ce683a22f5c74f43f9fbab05d
Maddumage Milinda Seneviratne <iamthemerovingian@hotmail.com>
2016-10-26 14:27:39 +0700

    The Department Collection has now been moved to its own .cs file.

--- CollectIt/Program.cs
+++ CollectIt/Program.cs
@@ -3,18 +3,7 @@ using System.Collections.Generic;
 
 namespace CollectIt
 {
-    public class DepartmentCollection : SortedDictionary<string, SortedSet<Employee>>
-    {
-        public DepartmentCollection Add(string departmentName, Employee employee)
-        {
-            if (!ContainsKey(departmentName))
-            {
-                Add(departmentName, new SortedSet<Employee>(new EmployeeComparer()));
-            }
-            this[departmentName].Add(employee);
-            return this;
-        }
-    }
+
     class Program
     {
         static void Main(string[] args)
